c6e8572b5420dc5d2dbf6fb37941310e3fee3a1f
vepifanio <vepifanio1@gmail.com>
2022-10-13 19:09:52 -0300

    feat: Login page created

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,6 +1,5 @@
-import { Slot } from "@radix-ui/react-slot";
-import { clsx } from "clsx";
 import { InputHTMLAttributes, ReactNode } from "react";
+import { Slot } from "@radix-ui/react-slot";
 
 export interface TextInputRootProps {
   children: ReactNode;
@@ -8,7 +7,7 @@ export interface TextInputRootProps {
 
 function TextInputRoot(props: TextInputRootProps) {
   return (
-    <div className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800  w-full focus-within:ring-2 ring-cyan-300">
+    <div className="flex items-center gap-3 h-12 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300">
       {props.children}
     </div>
   );
